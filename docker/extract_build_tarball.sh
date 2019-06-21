d8ff6ff7f605cf0aa03af0b6dce571c45bbc0f61
Hugh Cole-Baker <sigmaris@gmail.com>
2019-06-21 20:59:23 +0100

    Build arm64 deb packages with Docker using Drone

---
+++ docker/extract_build_tarball.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+RELEASE_DOWNLOAD_URL="https://github.com/sigmaris/xbmc/releases/download"
+
+if [ -z "$DRONE_TAG" ]
+then
+	echo "No DRONE_TAG, can't fetch build tarball."
+	exit 1
+fi
+
+KODI_TAG=${DRONE_TAG%"-addons"}
+curl -L -o /tmp/kodi-build.tar.bz2 "${RELEASE_DOWNLOAD_URL}/${KODI_TAG}/kodi-build.tar.bz2"
+tar xjvf /tmp/kodi-build.tar.bz2

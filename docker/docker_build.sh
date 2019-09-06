83a83365073227ef92f16ddadf237c576243bc53
Hugh Cole-Baker <sigmaris@gmail.com>
2019-09-06 20:35:41 +0100

    Further work on docker build

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,5 +1,28 @@
-#!/bin/sh
+#!/bin/sh -e
 
-docker build -t kodibuilder .
+. $(pwd)/buildconfig
 
-docker run -v `pwd`/..:/kodi-src -v `pwd`/../../kodi-build:/kodi-build kodibuilder
+if [ "$(uname -m)" == "aarch64" ]
+then
+	echo "Native aarch64 build..."
+	docker build \
+		--build-arg linux_libc_dev_release="$linux_libc_dev_release" \
+		--build-arg linux_libc_dev_sha256="$linux_libc_dev_sha256" \
+		--build-arg linux_libc_dev_file="$linux_libc_dev_file" \
+		-t kodibuilder \
+		.
+else
+	echo "Cross-platform build..."
+	docker buildx build \
+		--platform linux/arm64 \
+		--build-arg linux_libc_dev_release="$linux_libc_dev_release" \
+		--build-arg linux_libc_dev_sha256="$linux_libc_dev_sha256" \
+		--build-arg linux_libc_dev_file="$linux_libc_dev_file" \
+		-t kodibuilder \
+		--load \
+		.
+fi
+
+mkdir -p "$(pwd)/../../kodi-build"
+
+docker run -v "$(pwd)/..:/kodi-src" -v "$(pwd)/../../kodi-build:/kodi-build" kodibuilder

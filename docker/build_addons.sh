2ac68a3efd642d3ff63884763c82667ff2267b34
Hugh Cole-Baker <sigmaris@gmail.com>
2020-05-01 17:55:09 +0100

    sigmaris: build-files

---
+++ docker/build_addons.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+REPO_DIR="$(realpath ../kodi-src)"
+KODI_BUILD_DIR="$(pwd)"
+
+: "${ADDONS_BUILD_NUMBER:=$DRONE_BUILD_NUMBER}"
+
+echo "********************************************"
+echo "*** Installing binary addon dev packages ***"
+echo "********************************************"
+
+dpkg -i packages/kodi-addon-dev_*_all.deb packages/libkodiplatform*.deb
+
+echo "*********************************"
+echo "*** Configuring binary addons ***"
+echo "*********************************"
+
+mkdir -p build/addons_build
+cd build/addons_build
+cmake -DBUILD_DIR="$(pwd)" -DCORE_SOURCE_DIR="${REPO_DIR}" -DADDONS_TO_BUILD="${ADDONS_TO_BUILD}" -DADDON_DEPENDS_PATH="${KODI_BUILD_DIR}/build" "${REPO_DIR}/cmake/addons/"
+
+declare -a ADDONS_BUILD_OK
+declare -a ADDONS_BUILD_FAILED
+
+for D in $(ls . --ignore="*prefix")
+do
+	if [ -d "${D}/debian" ]
+	then
+		cd "${D}"
+		echo "*** Building binary addon $D ***"
+		VERSION_FILE="addon.xml.in"
+		[[ ! -f "${D}/addon.xml.in" ]] && VERSION_FILE="addon.xml"
+		ADDONS_PACK_VER=$(grep -oP "(  |\\t)version=\"(.*)\"" ./${D}/${VERSION_FILE} | awk -F'\"' '{print $2}')
+		sed -e "s/#PACKAGEVERSION#/${ADDONS_PACK_VER}/g" -e "s/#TAGREV#/${ADDONS_BUILD_NUMBER}/g" -e "s/#DIST#/${KODI_DISTRO_CODENAME}/g" debian/changelog.in > debian/changelog
+		if [[ $D == "pvr"* || $D == "audioencoder"* || $D == "visualization.waveform" ]]; then
+			for F in $(ls debian/*.install); do
+				echo "usr/lib" > ${F}
+				echo "usr/share" >> ${F}
+			done
+		fi
+		# if [[ $D == "audioencoder"* || $D == "audiodecoder"* ]]; then
+		# 	sed -i "s/-DUSE_LTO=1//g" debian/rules
+		# fi
+
+		dpkg-buildpackage -us -uc -b --jobs=auto
+		if [ $? -ne 0 ]
+		then
+			ADDONS_BUILD_FAILED+=("${D}")
+		else
+			ADDONS_BUILD_OK+=("${D}")
+		fi
+		cd ..
+	fi
+done
+
+echo "********************************"
+echo "*** Finished building addons ***"
+echo "********************************"
+echo ""
+echo "Addons built OK: ${ADDONS_BUILD_OK[@]}"
+echo ""
+echo "Addons which failed to build: ${ADDONS_BUILD_FAILED[@]}"
---
+++ docker/build_kodi.sh
@@ -0,0 +1,89 @@
+#!/bin/bash -e
+
+REPO_DIR="$(realpath ../kodi-src)"
+KODI_BUILD_DIR="$(pwd)"
+
+: "${KODI_BUILD_NUMBER:=$DRONE_BUILD_NUMBER}"
+: "${KODI_PLATFORM_BUILD_NUMBER:=$DRONE_BUILD_NUMBER}"
+: "${KODI_DISTRO_CODENAME:=unknown}"
+
+if [[ "$KODI_DISTRO_CODENAME" == "bullseye" ]]
+then
+  INTERNAL_FLATBUFFERS=OFF
+else
+  INTERNAL_FLATBUFFERS=ON
+fi
+
+echo "************************"
+echo "*** Configuring kodi ***"
+echo "************************"
+
+cmake "${REPO_DIR}" \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCORE_PLATFORM_NAME=gbm \
+  -DGBM_RENDER_SYSTEM=gles \
+  -DENABLE_OPENGL=OFF \
+  -DENABLE_OPENGLES=ON \
+  -DENABLE_AIRTUNES=ON \
+  -DENABLE_ALSA=ON \
+  -DENABLE_AVAHI=ON \
+  -DENABLE_BLURAY=ON \
+  -DENABLE_CEC=ON \
+  -DENABLE_DBUS=ON \
+  -DENABLE_DVDCSS=ON \
+  -DENABLE_EGL=ON \
+  -DENABLE_EVENTCLIENTS=ON \
+  -DENABLE_INTERNAL_FFMPEG=OFF \
+  -DENABLE_V4L2=ON \
+  -DENABLE_INTERNAL_CROSSGUID=OFF \
+  -DENABLE_INTERNAL_FLATBUFFERS="${INTERNAL_FLATBUFFERS}" \
+  -DENABLE_MICROHTTPD=ON \
+  -DENABLE_MYSQLCLIENT=ON \
+  -DENABLE_NFS=ON \
+  -DENABLE_OPENSSL=ON \
+  -DENABLE_OPTICAL=ON \
+  -DENABLE_PULSEAUDIO=ON \
+  -DENABLE_SMBCLIENT=ON \
+  -DENABLE_SSH=ON \
+  -DENABLE_UDEV=ON \
+  -DENABLE_UPNP=ON \
+  -DENABLE_XSLT=ON \
+  -DENABLE_LIRC=ON \
+  -DCPACK_GENERATOR=DEB \
+  -DDEBIAN_PACKAGE_VERSION="${KODI_BUILD_NUMBER}~" \
+  -DDEB_PACKAGE_ARCHITECTURE=arm64 \
+  -DDEBIAN_PACKAGE_TYPE=unstable \
+  -DDISTRO_CODENAME="${KODI_DISTRO_CODENAME}"
+
+echo "*********************"
+echo "*** Building kodi ***"
+echo "*********************"
+
+cmake --build . -- -j$(getconf _NPROCESSORS_ONLN)
+
+echo "**************************"
+echo "*** Building kodi debs ***"
+echo "**************************"
+
+cpack
+
+echo "********************************************"
+echo "*** Installing binary addon dev packages ***"
+echo "********************************************"
+
+dpkg -i packages/kodi-addon-dev_*_all.deb
+
+echo "*************************************"
+echo "*** Building libkodiplatform debs ***"
+echo "*************************************"
+
+cd packages
+git clone https://github.com/xbmc/kodi-platform.git
+cd kodi-platform
+sed -e "s/#TAGREV#/${KODI_PLATFORM_BUILD_NUMBER}/g" -e "s/#DIST#/$(lsb_release -cs)/g" debian/changelog.in > debian/changelog
+dpkg-buildpackage -us -uc -b --jobs=auto
+
+echo "*************"
+echo "*** Done! ***"
+echo "*************"

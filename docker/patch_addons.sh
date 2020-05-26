d40b81d26ada7759875d6186c750bf56417178a5
Hugh Cole-Baker <sigmaris@gmail.com>
2020-05-26 21:27:11 +0100

    Only apply patches if some exist

--- docker/patch_addons.sh
+++ docker/patch_addons.sh
@@ -10,7 +10,10 @@ echo "*****************************"
 cd build/addons_build
 for patch in ${REPO_DIR}/docker/patches/addons/${ADDONS_TYPE}/*.patch
 do
-	patch -p1 < $patch
+	if [ -f "$patch" ]
+	then
+		patch -p1 < $patch
+	fi
 done
 
 echo "***********************"

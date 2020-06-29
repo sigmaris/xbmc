082cc15027042917094dd86bd771b80445818ff7
Hugh Cole-Baker <sigmaris@gmail.com>
2020-06-29 22:42:32 +0100

    Publish non-tag build artifacts to bintray

---
+++ docker/publish_bintray.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+: "${BINTRAY_USER:=sigmaris}"
+
+kodi_version="$(set -- kodi_*_all.deb; echo "$1" | cut -d '_' -f 2)"
+if [[ -z "$kodi_version" ]]
+then
+	echo "Can't detect Kodi version from these files:"
+	ls
+	exit 1
+fi
+
+echo "*********************************"
+echo "*** Uploading built artifacts ***"
+echo "*********************************"
+
+for pkgfile in *.deb
+do
+	echo " ${pkgfile}..."
+	curl -T "$pkgfile" --netrc-file <(cat <<<"machine api.bintray.com login $BINTRAY_USER password $BINTRAY_API_KEY") "https://api.bintray.com/content/${BINTRAY_USER}/artifacts/kodi/${kodi_version}/${pkgfile}"
+done
+
+echo "*************"
+echo "*** Done! ***"
+echo "*************"

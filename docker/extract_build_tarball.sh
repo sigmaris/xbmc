0678dbf7db83edd29eb6436f6ce9aee5d64e640d
Hugh Cole-Baker <sigmaris@gmail.com>
2020-11-21 18:51:18 +0000

    Find kodi tag from games tag

--- docker/extract_build_tarball.sh
+++ docker/extract_build_tarball.sh
@@ -9,6 +9,12 @@ then
 	exit 1
 fi
 
-KODI_TAG=${DRONE_TAG%-addons}
+if [[ "$DRONE_TAG" == *-games ]]
+then
+	KODI_TAG=${DRONE_TAG%-games}
+else
+	KODI_TAG=${DRONE_TAG%-addons}
+fi
+
 curl -L -o /tmp/addons-dev.tar.bz2 "${RELEASE_DOWNLOAD_URL}/${KODI_TAG}/addons-dev-${KODI_DISTRO_CODENAME}.tar.bz2"
 tar xjvf /tmp/addons-dev.tar.bz2
